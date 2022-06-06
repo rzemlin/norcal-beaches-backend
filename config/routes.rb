75e1c9b2ab50a0470195ef453309fdbdc9323e35
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-06-06 16:29:04 -0700

    add post routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :beaches, only: [:index, :create, :show, :edit, :destroy]
       resources :comments, only: [:index, :create, :show, :destroy]
+      get "/beaches", to: "beaches#index"
+      post "/beaches", to: "beaches#create"
     end
   end
 end
